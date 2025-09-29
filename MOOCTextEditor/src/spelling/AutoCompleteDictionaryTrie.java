e8bc6eb01239a3ed7738e8ce1cbcb83aafee9325
Agathe Helman <ahelman@yseop.com>
2025-09-29 16:41:52 +0200

    feat: week5

--- MOOCTextEditor/src/spelling/AutoCompleteDictionaryTrie.java
+++ MOOCTextEditor/src/spelling/AutoCompleteDictionaryTrie.java
@@ -1,9 +1,8 @@
 package spelling;
 
 import java.util.List;
-import java.util.Set;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.Queue;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /** 
@@ -37,32 +36,68 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
 	 * @return true if the word was successfully added or false if it already exists
 	 * in the dictionary.
 	 */
-	public boolean addWord(String word)
-	{
-	    //TODO: Implement this method.
-	    return false;
+	public boolean addWord(String word) {
+		// Week5 assignment.
+		if (word == null || word.isEmpty()) {
+			return false;
+	    }
+
+	    word = word.toLowerCase();
+	    TrieNode current = root;
+
+	    for (char c : word.toCharArray()) {
+	    	TrieNode next = current.getChild(c);
+	        if (next == null) {
+	        	next = current.insert(c);
+	        }
+	        current = next;
+	    }
+
+	    if (current.endsWord()) {
+	    	return false; // Word already exists
+	    }
+
+	    current.setEndsWord(true);
+	    size++;
+	    return true;
 	}
+	
 	
 	/** 
 	 * Return the number of words in the dictionary.  This is NOT necessarily the same
 	 * as the number of TrieNodes in the trie.
 	 */
-	public int size()
-	{
-	    //TODO: Implement this method
-	    return 0;
+	public int size() {
+	    // Week5 assignment.
+		
+	    return size;
 	}
 	
 	
 	/** Returns whether the string is a word in the trie, using the algorithm
 	 * described in the videos for this week. */
 	@Override
-	public boolean isWord(String s) 
-	{
-	    // TODO: Implement this method
-		return false;
+	public boolean isWord(String s) {
+		// Week5 assignment.
+		
+		if (s == null || s.isEmpty()) {
+			return false;
+	    }
+
+	    s = s.toLowerCase();
+	    TrieNode current = root;
+
+	    for (char c : s.toCharArray()) {
+	    	current = current.getChild(c);
+	    	if (current == null) {
+	    		return false;
+	        }
+	    }
+
+	    return current.endsWord();
 	}
 
+	
 	/** 
      * Return a list, in order of increasing (non-decreasing) word length,
      * containing the numCompletions shortest legal completions 
@@ -80,30 +115,59 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
      * 
      * If this string prefix is not in the trie, it returns an empty list.
      * 
-     * @param prefix The text to use at the word stem
-     * @param numCompletions The maximum number of predictions desired.
+     * @param prefix 			The text to use at the word stem
+     * @param numCompletions 	The maximum number of predictions desired.
      * @return A list containing the up to numCompletions best predictions
      */@Override
-     public List<String> predictCompletions(String prefix, int numCompletions) 
-     {
-    	 // TODO: Implement this method
+     public List<String> predictCompletions(String prefix, int numCompletions) {
+    	 // Week5 assignment.
+    	 
+    	 List<String> completions = new ArrayList<>();
+    	 if (numCompletions == 0) return completions;
+
+    	 prefix = prefix.toLowerCase();
+    	 TrieNode current = root;
+    	    
     	 // This method should implement the following algorithm:
-    	 // 1. Find the stem in the trie.  If the stem does not appear in the trie, return an
-    	 //    empty list
+    	 // 1. Find the stem in the trie.  
+    	 // If the stem does not appear in the trie, return an empty list
+    	 for (char c : prefix.toCharArray()) {
+    		 current = current.getChild(c);
+    		 if (current== null) {
+    			 return completions; // Prefix not found
+	        }
+	     }
+    	 
     	 // 2. Once the stem is found, perform a breadth first search to generate completions
     	 //    using the following algorithm:
     	 //    Create a queue (LinkedList) and add the node that completes the stem to the back
     	 //       of the list.
+    	 Queue<TrieNode> nodeQueue = new LinkedList<>();
+ 	     nodeQueue.add(current);
+
     	 //    Create a list of completions to return (initially empty)
     	 //    While the queue is not empty and you don't have enough completions:
     	 //       remove the first Node from the queue
     	 //       If it is a word, add it to the completions list
     	 //       Add all of its child nodes to the back of the queue
+
+ 	     while (!nodeQueue.isEmpty() && completions.size() < numCompletions) {
+ 	    	 TrieNode node = nodeQueue.poll();
+ 	    	 
+ 	    	 if (node.endsWord()) {
+ 	    		 completions.add(node.getText());
+ 	    	 }
+
+ 	    	 for (Character c : node.getValidNextCharacters()) {
+ 	    		 nodeQueue.add(node.getChild(c));
+ 	    	 }
+    	 }
     	 // Return the list of completions
     	 
-         return null;
+ 	     return completions;     
      }
 
+     
  	// For debugging
  	public void printTree()
  	{
